487fd6d44afc19019b961cb5de5e02b201765367
Maciej Siwicki <maciej.siwicki00@gmail.com>
2024-05-25 10:19:39 +0200

    add frontend project

---
+++ model/convert_model.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import tensorflowjs as tfjs
+
+# Load your model
+model = tf.keras.models.load_model("ThrashRecognitionML.v1.ipynb.keras")
+
+# Save the model in TensorFlow.js format
+tfjs.converters.save_keras_model(model, "path_to_save_tfjs_model")
